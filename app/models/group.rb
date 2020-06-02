70829c86ee36406b778e82baac0786551bcae81d
junkiyamada <65000192+junkiyamada@users.noreply.github.com>
2020-06-02 17:03:27 +0900

    add-chat-fanction

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,19 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+  has_many :posts
   validates :name, presence: true, uniqueness: true
+
+  def show_last_post
+    if (last_post = posts.last).present?
+      if last_post.content?
+        last_post.content
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージがありません'
+    end
+  end
+
 end
